7beb3908c1e0f3713e18f87a216f5b577241dfc4
root <root@ip-172-31-90-174.ec2.internal>
2025-05-10 04:55:52 +0000

    update new key

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIA4MTWH6SIFXY47EHR"
-secret_key = "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"
+access_key = "AKIA4MTWH6SILL2I7VMN"
+secret_key = "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"
 }
 
 resource "aws_instance" "key" {

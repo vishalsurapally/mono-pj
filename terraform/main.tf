9a713a28fd883fe2493b16339f304f4f0136afcf
root <root@ip-172-31-93-163.ec2.internal>
2025-05-19 05:30:22 +0000

    terraform main.tf code change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIA4MTWH6SILL2I7VMN"
-secret_key = "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"
+access_key = "AKIA4MTWH6SIDDURDYF7"
+secret_key = "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"
 }
 
 resource "aws_instance" "key" {
-ami = "ami-085386e29e44dacd7"
+ami = "ami-0e58b56aa4d64231b"
 key_name = "mykey"
 instance_type = "t2.micro"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]

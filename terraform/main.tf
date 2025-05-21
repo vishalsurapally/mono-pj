a9f4e9004e206b9312b9fb32c1f7840bee849ca5
vishalsurapally <94595179+vishalsurapally@users.noreply.github.com>
2025-05-21 19:18:18 +0530

    Update keys

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,8 @@ secret_key = "O5t/FTkP4jcl9/T+JzhV8TAWDzxy2z9BDM0humPH"
 
 resource "aws_instance" "key" {
 ami = "ami-0e58b56aa4d64231b"
-key_name = "mykey"
 instance_type = "t2.micro"
+key_name = "mykey"
 vpc_security_group_ids = [aws_security_group.demo-sg01.id]
 tags = {
 Name = "project-instance"
